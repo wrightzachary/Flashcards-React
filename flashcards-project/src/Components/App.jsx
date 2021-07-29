d815231acd5d044b6cd29ce250de5ca5dd08c4eb
Zachary <taylorzw96@gmail.com>
2021-07-29 10:50:33 -0500

    Adding TitleBar Compobebt

--- flashcards-project/src/Components/App.jsx
+++ flashcards-project/src/Components/App.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import TitleBar from './TtitleBar/TitleBar';
 
 class App  extends Component {
     constructor(props) {
@@ -11,7 +12,7 @@ class App  extends Component {
     render() { 
         return (  
             <React.Fragment>
-                <h1>Flashcard Application</h1>
+                <TitleBar />
             </React.Fragment>
         );
     }
---
+++ flashcards-project/src/Components/TtitleBar/TitleBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const TitleBar = (props) => {
+    return ( 
+        <React.Fragment>
+            <div className="row row-spacer">
+                <h1>dCC Flashcards</h1>
+            </div>
+        </React.Fragment>
+     );
+}
+ 
+export default TitleBar;
